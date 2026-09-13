7b27cae35c82b10ec59ee33bd879cfb8e98c5007
pmuwanguzi <p.muwanguzi@alustudent.com>
2026-09-13 15:10:20 -0700

    updated

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,21 +7,18 @@
  */
 int main(void)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; i <= 8; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = i + 1; j <= 9; j++)
+		putchar(i + '0');
+		if (i < 9)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			if (i != 8 || j != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
+
 	putchar('\n');
 
 	return (0);
