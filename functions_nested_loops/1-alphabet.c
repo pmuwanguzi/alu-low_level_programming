cb61161fdab1731b02c06e97536b78500775016c
pmuwanguzi <p.muwanguzi@alustudent.com>
2026-09-21 15:23:16 -0700

    formating

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ *
+ * Return: void (nothing)
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
